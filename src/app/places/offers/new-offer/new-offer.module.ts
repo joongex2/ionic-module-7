693196bd7a2473b8dbff8e549fafb7956f84c0ca
Supanat Auengsirirat <joong555@gmail.com>
2023-07-24 12:09:29 +0700

    feat: module-9, module-10
    - add some global styles
    - add theme variable (toolbar color, background)
    - add template driven form (auth, create-booking)
    - add reactive form (edit-offer, new-offer)
    - refactor use offer-item loop in offer page

--- src/app/places/offers/new-offer/new-offer.module.ts
+++ src/app/places/offers/new-offer/new-offer.module.ts
@@ -1,20 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { NewOfferPageRoutingModule } from './new-offer-routing.module';
-
 import { NewOfferPage } from './new-offer.page';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     NewOfferPageRoutingModule
   ],
   declarations: [NewOfferPage]
 })
-export class NewOfferPageModule {}
+export class NewOfferPageModule { }
